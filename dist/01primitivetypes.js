a1aa28fdf21f55631d7fa64e4efdd30a575c4d1e
kth0027 <so_dream@naver.com>
2022-03-17 19:35:02 +0900

    업로드

---
+++ dist/01primitivetypes.js
@@ -0,0 +1,14 @@
+let 이름 = 'kim';
+// 이름 = 123; 에러
+let 나이 = 50;
+let 결혼했니 = true;
+// undefined, null 타입도 있음
+// array 자료에 타입지정 가능
+let 회원들1 = ['kim', 'park'];
+// 혼용사용시 UnionType
+// object 자료에도 타입 지정 가능
+let 회원들2 = { member1: 'kim', member2: 'park' };
+// 타입지정 원래 자동으로 된다. 타입지정 문법 생략 가능.
+let 회원들 = 'park';
+let 회원들3 = 123;
+//# sourceMappingURL=01primitivetypes.js.map
---
+++ dist/index.js
@@ -0,0 +1,19 @@
+// 변수에 타입 지정
+let 이름1 = 'kim';
+let 이름2 = ['kim', 'park'];
+let 이름3 = { name: "kim" };
+let 이름4 = 123;
+let 이름5 = 123;
+let 이름6 = 123;
+// 함수
+function 함수(x) {
+    return x * 2;
+}
+let john = [123, true];
+let john2 = { name: 'kim' };
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+//# sourceMappingURL=index.js.map
